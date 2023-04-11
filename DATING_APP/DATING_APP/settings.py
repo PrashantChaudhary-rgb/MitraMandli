8be17824261e8aed0e4f1f2a4944e5cfc5af7dfe
PrashantChaudhary-rgb <73767749+PrashantChaudhary-rgb@users.noreply.github.com>
2023-04-11 14:23:21 +0530

    created routes for login , register and home

--- DATING_APP/DATING_APP/settings.py
+++ DATING_APP/DATING_APP/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'spotify_test_app.apps.SpotifyTestAppConfig',
+    'users.apps.UsersConfig',
+    'dating.apps.DatingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DATING_APP/DATING_APP/urls.py
+++ DATING_APP/DATING_APP/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path , include
+from users import views as userViews
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('',include('dating.urls')),
     path('admin/', admin.site.urls),
-    path('', include('spotify_test_app.urls')),
+    path('register/', userViews.register , name = 'register'),
+    path('login/', auth_views.LoginView.as_view( template_name = 'users\login.html') , name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view( template_name = 'users\logout.html') , name = 'logout'),
+    
 
 ]
